30021d650ad0f8d384abc11015d9b217232a142a
Sumit Dahiya <sumitdahiya38@gmail.com>
2020-06-10 12:59:47 +0530

    dbservice changes

--- src/lib/db-service.js
+++ src/lib/db-service.js
@@ -1,48 +1,28 @@
-import mongoose from 'mongoose';
 import * as _ from 'lodash';
-
+import constants from '../constants/app-constant'
 export default class DbService {
     constructor(model){
         this._model = model;
     }
 
-    async find(params) {
-        let obj = await this._model.find(params);
-        obj = obj.map(item => _.pick(item,Object.keys(this._model.schema.paths)))        
-        return obj;
+    async find(queryObj,params) {
+        return this._model.find(queryObj,params,{skip:constants.properties.skip,limit:constants.properties.limit});
     }
 
     async get(id){
-        let obj = await this._model.findOne({
-            _id:mongoose.Types.ObjectId(id)
-        });
-        obj = _.pick(obj,Object.keys(this._model.schema.paths))       
-        return obj;
+        return await this._model.findOne({_id:id});
     }
 
-    async create(data) {
-        let obj = this._model(data);
-        return new Promise((resolve,reject)=>{
-            obj.save((err,response)=>{
-                if(err){
-                    throw new Error(err);
-                }
-                let filtered = _.pick(response,Object.keys(this._model.schema.paths));
-                resolve(filtered);
-            });
-        })
+    async create(data,params) {
+        return this._model.create(data);
     }
 
     async remove(id, params) {
-        return this._model.remove({
-            _id:mongoose.Types.ObjectId(id)
-        },params)
+        return this._model.remove({_id:id},params)
     }
 
     async update(id, data, params) {
-        let updatedObj = this._model.updateMany({
-            _id:mongoose.Types.ObjectId(id)
-        },data,params);
-        return updatedObj;
+        return this._model.findOneAndUpdate({_id:id},data,params);
     }
+
 }
