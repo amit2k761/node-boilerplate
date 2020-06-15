2b8239f5f513863c5221b02f4396040cb3ee54fd
Sumit Dahiya <sumitdahiya38@gmail.com>
2020-06-15 13:56:08 +0530

    sequelize file

---
+++ src/lib/sequelize-service.js
@@ -0,0 +1,42 @@
+import * as _ from 'lodash';
+
+export default class SequelizeService {
+
+    async find(queryObj = {}, params = {}) {
+        let { limit, skip, select } = params;
+        params = _.omit(params, ['limit', 'skip', 'select']);
+        return this._model.findAll({
+            where:select,
+            attributes:queryObj,
+            limit: +limit || 10 ,
+            offset: +skip || 0
+        });
+    }
+
+    async get(id,params){
+        return await this._model.findOne({where:{id:id},attributes:params});
+    }
+
+    async create(data,params) {
+        return this._model.bulkCreate(data, {
+            returning: true,
+            fields: params
+        }); 
+    }
+
+    async remove(id, params) {
+        return this._model.destroy({
+            where:{id:id},
+            attributes:params,
+            returning:true
+        })
+    }
+
+    async update(id, data, params) {
+        return this._model.update(data,{
+            where: {id: id},
+            attributes:params,
+            returning:true
+        },params);
+    }
+}
