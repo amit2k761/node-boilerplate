b4c0aeeacd8513ef1e71d68eb5e56eada12e2081
Sumit Dahiya <sumitdahiya38@gmail.com>
2020-06-17 15:48:37 +0530

    validation changes

--- src/utils/handle-validation-error.js
+++ src/utils/handle-validation-error.js
@@ -1,9 +1,9 @@
 import HttpException from './http-exception';
+import Joi from '@hapi/joi';
 
-export default (req, valSchema) => {
+export default  (req, valSchema,type) => {
   try {
-    const { error, value, warning } = valSchema.validate(req.body);
-
+    const { error, value, warning } = type ? Joi.array().items(valSchema).min(1).required().validate(req.body):valSchema.validate(req.body);
     if (!error) return { errorMessage: '', value };
 
     const errorMessage =
